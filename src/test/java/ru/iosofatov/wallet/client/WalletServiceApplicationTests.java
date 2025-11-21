76e633708ed5fb936d635f372bb8ec9039cf7e6a
iosik89 <golemios89@gmail.com>
2025-11-21 16:41:48 +0300

    1

---
+++ src/test/java/ru/iosofatov/wallet/client/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iosofatov.wallet.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
